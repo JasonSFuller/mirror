62e14f230168b94b696d346666dd286d447cadf2
Jason Fuller <JasonSFuller@gmail.com>
2020-05-26 22:49:33 -0400

    feat: walk dir looking for etc/mirror.conf

--- bin/inc/read_config.sh
+++ bin/inc/read_config.sh
@@ -1,27 +1,31 @@
 # this is only intended be referenced by scripts in the bin/
 # directory, but it could be expanded.
 #
-# TODO **maybe?** walk back up to root looking for ./etc/mirror.conf:
-#   while [[ $PWD != / ]]; do
-#     cd ..
-#     # do stuff
-#   done
-
-# TODO validate required vars
+# TODO validate vars from mirror.conf, rather than just reading them in
 
 function read_config
 {
   local self=$(realpath -e "${BASH_SOURCE[0]}")
   local selfdir=$(dirname "$self")
 
-  MIRROR_CONFIG="${selfdir}/../etc/mirror.conf"
+  MIRROR_CONFIG="${selfdir}/etc/mirror.conf"
+  pushd . &>/dev/null # save current location
+  cd "$selfdir"
 
-  if [[ -r "${MIRROR_CONFIG}" ]]; then
-    source "${MIRROR_CONFIG}"
-  else
-    echo "ERROR: could not read config (${MIRROR_CONFIG})" 2>&1
-    exit 1
-  fi
+  for ((i=0; i<100; i++)) # in case of fs loops
+  do
+    if [[ -r "${MIRROR_CONFIG}" ]]; then
+      source "${MIRROR_CONFIG}"
+      popd &>/dev/null
+      return
+    fi
+    if [[ "$PWD" == '/' ]]; then break; fi
+    cd ..
+    MIRROR_CONFIG="$PWD/etc/mirror.conf"
+  done
+
+  echo "ERROR: missing config" 2>&1
+  exit 1
 }
 
 read_config
